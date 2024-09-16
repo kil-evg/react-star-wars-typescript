2cabc466c49e972e5981c1f6c3a182d36fbf4eac
EKil <loreleyko@gmail.com>
2024-09-16 23:14:49 +0200

    Homework09

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from 'react'
-import { base_url, period_month } from '../utils/constants'
-import { HeroInfo } from '../utils/types';
+import { characters, period_month } from '../utils/constants'
+import { Hero, HeroInfo } from '../utils/types';
 import { useParams } from 'react-router-dom';
 
 const AboutMe = () => {
   const [hero, setHero] = useState<HeroInfo>();
-  const {heroId} =  useParams();
+  const { heroId } = useParams();
   console.log(heroId);
+  const aboutHero: Hero = characters.find(h => h.id === heroId)! || characters.find(h => h.id === 'luke');
 
   useEffect(() => {
-    const hero = JSON.parse(localStorage.getItem('hero')!);
+    const hero = JSON.parse(localStorage.getItem(aboutHero.id)!);
     if (hero && ((Date.now() - hero.time) < period_month)) {
       setHero(hero.payload)
     }
     else {
-      fetch(`${base_url}/v1/peoples/1`)
+      fetch(aboutHero.url)
         .then(response => response.json())
         .then(data => {
           const info = {
@@ -28,7 +29,7 @@ const AboutMe = () => {
             eye_color: data.eye_color
           }
           setHero(info);
-          localStorage.setItem('hero', JSON.stringify({
+          localStorage.setItem(aboutHero.id, JSON.stringify({
             time: Date.now(),
             payload: info
           }))
@@ -39,11 +40,13 @@ const AboutMe = () => {
 
   return (
     <>
+
       {(!!hero) &&
         <div className='text-3xl leading-loose text-justify tracking-widest ml-8'>
           {Object.keys(hero).map(key => <p key={key}><span className='text-[1.5em] capitalize'>{key.replace('_', ' ')}:</span> {hero[key as keyof HeroInfo]}</p>)}
         </div>
       }
+
     </>
   )
 }
