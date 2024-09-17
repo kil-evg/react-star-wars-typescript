7546aad5855ddab68fc0ca82f7f6ecacb570fde1
EKil <loreleyko@gmail.com>
2024-09-17 09:46:19 +0200

    change AboutMe by heroId

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -18,3 +18,12 @@ export interface Item {
     title: string,
     path: string
 }
+
+export interface Hero {
+    name: string,
+    img: string,
+    url: string
+}
+export interface Characters {
+    [key: string]: Hero
+}
